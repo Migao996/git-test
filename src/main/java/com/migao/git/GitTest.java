29d1da09eade5d40192f375a7efb1372eb7d3c01
Migao <qi1030422412@163.com>
2023-01-16 17:43:46 +0800

    first commit

---
+++ src/main/java/com/migao/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.migao.git;
+
+/**
+ * @author Qi
+ * @create 2023-01-16 17:40
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
